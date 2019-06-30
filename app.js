9a8b9fa7aaacb1988d83e011e64d10300824a85d
Steven <stevenyan53@gmail.coim>
2019-06-30 22:56:48 +1200

    Bug fix, now the html gets updated after add data

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var budgetController = (function() {
     
     var data = {
         allItems:{
-            exp: [];
+            exp: [],
             inc: []
-        }
-        totals = {
+        },
+        totals: {
             exp: 0,
             inc: 0
         }
@@ -48,10 +48,7 @@ var budgetController = (function() {
             return newItem;
         }
     }
-        
-    };
-    
-})();
+    })();
 
 
 
@@ -62,7 +59,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
     
     
@@ -77,11 +76,26 @@ var UIController = (function() {
         },
         
         addListItem: function(obj, type){
-            
+            var html, newHtml, element;
+            // Create HTML string
+            if(type === 'inc'){
+            element = DOMstrings.incomeContainer;
+            html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if(type === 'exp'){
+            element = DOMstrings.expensesContainer;
+            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // replace the placeholders with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            // insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
         },
         
         
-        getDOMstring : function(){
+        
+        getDOMstring: function(){
             return DOMstrings;
         }
     };
@@ -113,6 +127,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         input = UICtrl.getInput();
         //add to data stucture
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        //add to ui
+        UICtrl.addListItem(newItem, input.type);
         console.log(input);
     }
     return {
